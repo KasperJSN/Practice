8962e7f42979bfc38c48c04f95bd0141edbc0b01
Jan Pajdak <pajdakjan@gmail.com>
2018-01-31 18:40:56 +0100

    Init

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,21 @@ namespace Basics
     {
         static void Main(string[] args)
         {
+            TestMethod();
+
+            Console.ReadKey(); // required so console window does not close automatically; its waiting for user input (reading a key)
+        }
+
+        static void TestMethod()
+        {
+            // outputs text to console window
+            // WriteLine takes entire line; Write appends
+            // "\n" is a special new line symbol
+            // WriteLine is basically Write + invisible "\n" at the end
+            Console.WriteLine("test message 1");
+            Console.Write("test ");
+            Console.Write("message 2");
+            Console.Write("test message 3 \n");
         }
     }
 }
